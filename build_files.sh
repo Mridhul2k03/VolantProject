d648d227cedc46f1af2abae2b357820c7766fe05
Mridhul2k03 <mridhulkrishna11@gmail.com>
2024-06-27 11:13:38 +0530

    update commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
- echo "BUILD START"
- python3.12.1 -m pip install -r requirements.txt
- python3.12.1 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+#!/bin/bash
+
+echo "BUILD START"
+python3.12.1 -m pip install -r requirements.txt
+python3.12.1 manage.py collectstatic --noinput --clear
+echo "BUILD END"
